cd31714e848995e763777c0654f022ce0def0da7
Yoriel <yoriel.silva.rodrigues@gmail.com>
2025-05-14 21:10:53 -0300

    arrumar bottom sheet

--- app/src/main/java/com/example/ryno/DetalhesProfessorBottomSheet.kt
+++ app/src/main/java/com/example/ryno/DetalhesProfessorBottomSheet.kt
@@ -16,16 +16,16 @@ class DetalhesProfessorBottomSheet(private val professor: Professor) : BottomShe
         val view = inflater.inflate(R.layout.bottom_sheet_professor, container, false)
 
         view.findViewById<TextView>(R.id.txtNome).text = professor.nome
-        view.findViewById<TextView>(R.id.txtEmail).text = "Email: ${professor.email}"
-        view.findViewById<TextView>(R.id.txtTelefone).text = "Telefone: ${professor.telefone}"
-        view.findViewById<TextView>(R.id.txtCidade).text = "Cidade: ${professor.cidade}"
+        view.findViewById<TextView>(R.id.txtEmail).text = "${professor.email}"
+        view.findViewById<TextView>(R.id.txtTelefone).text = "${professor.telefone}"
+        view.findViewById<TextView>(R.id.txtCidade).text = "${professor.cidade}"
 
         val crefTextView = view.findViewById<TextView>(R.id.txtCref)
         val crefLink = "https://www.confef.org.br"
-        crefTextView.text = android.text.Html.fromHtml("<a href=\"$crefLink\">CREF: ${professor.cref}</a>")
+        crefTextView.text = android.text.Html.fromHtml("<a href=\"$crefLink\">${professor.cref}</a>")
         crefTextView.movementMethod = android.text.method.LinkMovementMethod.getInstance()
 
-        view.findViewById<TextView>(R.id.txtModalidades).text = "Modalidades: ${professor.modalidades.joinToString(", ")}"
+        view.findViewById<TextView>(R.id.txtModalidades).text = "${professor.modalidades.joinToString(", ")}"
 
         professor.profileImageUrl?.let {
             Picasso.get().load(it).transform(CropCircleTransformation()).into(view.findViewById(R.id.imgFoto) as ImageView)
